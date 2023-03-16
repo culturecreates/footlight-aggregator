d352fb256c17370ac6290eeb0bb83cac7f0e842f
suhailaliyar <suhailaliyar.mec@gmail.com>
2023-03-16 13:52:45 +0530

    Added support to format alternateNames before pushing.

---
+++ src/module/api/dto/shared/shared-dto.ts
@@ -0,0 +1,8 @@
+import {IdentifierString, UriString} from "../../model";
+
+export class Image {
+    original: IdentifierString;
+    large: IdentifierString;
+    thumbnail: IdentifierString;
+    url?: UriString;
+}
