aba4bf5315eead91a1829f5e3cff1df28b6217f7
Gregory Saumier-Finch <gregory@culturecreates.com>
2024-05-08 16:41:42 -0400

    use v2 sparqls that allow external place uris

--- src/module/api/constants/artsdata-urls/artsdata-urls.constants.ts
+++ src/module/api/constants/artsdata-urls/artsdata-urls.constants.ts
@@ -1,14 +1,8 @@
 export const ArtsDataUrls = {
-  EVENTS: "http://api.artsdata.ca/query?sparql=query_footlight_events&frame=event_footlight&format=json",
+  EVENTS: "http://api.artsdata.ca/query?sparql=https://raw.githubusercontent.com/culturecreates/footlight-aggregator/main/sparql/query-events-v2.sparql&frame=event_footlight&format=json",
   EVENT_SERIES: "https://api.artsdata.ca/query?sparql=https://raw.githubusercontent.com/culturecreates/footlight-aggregator/main/sparql/query-event-series.sparql&frame=event_footlight&format=json",
-  PLACE_BY_ID: "http://api.artsdata.ca/query?adid=ARTS_DATA_ID&format=json&frame=ranked_place_footlight&sparql=ranked_place_footlight",
+  PLACE_BY_ID: "http://api.artsdata.ca/query?uri=ARTS_DATA_ID&format=json&frame=ranked_place_footlight&sparql=https://raw.githubusercontent.com/culturecreates/footlight-aggregator/main/sparql/query-place-v2.sparql",
   PERSON_ORGANIZATION_BY_ID: "http://api.artsdata.ca/query?adid=ARTS_DATA_ID&format=json&frame=ranked_org_person_footlight&sparql=ranked_org_person_footlight",
-
-  // PEOPLE: 'https://api.artsdata.ca/person?format=json',
-  // PLACES: 'https://api.artsdata.ca/place?format=json',
-  // ORGANIZATIONS: 'https://api.artsdata.ca/organizations?format=json',
-  // PERSON_BY_ID: 'http://api.artsdata.ca/ranked/ARTS_DATA_ID?format=json&frame=ranked_person_footlight',
-  // ORGANIZATION_BY_ID: 'http://api.artsdata.ca/ranked/ARTS_DATA_ID?format=json&frame=ranked_org_footlight',
   ARTSDATA_SPARQL_ENDPOINT: "https://db.artsdata.ca/repositories/artsdata"
 };
 
