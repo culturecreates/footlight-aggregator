c2bd0cd520544e693d3adb028e42ef4684dd1167
sahalali <sahal@culturecreates.com>
2024-12-11 11:24:15 +0530

    Beautify logs

--- script/sync-artsdata-ids/sync-artsdata-ids.py
+++ script/sync-artsdata-ids/sync-artsdata-ids.py
@@ -15,40 +15,40 @@ def load_query(file_path):
     with open(file_path, 'r') as file:
         return file.read()
 
-def append_ids( results):
+def append_ids(results):
     ids = ''
     for result in results:
         ids = ids + "resource:" + str(result['_id']) + "\n"
     return ids
 
-
 def get_entity_ids(client, collection):
     db = client['footlight-calendar']
     results = db[collection].aggregate([
-            {"$match": {"sameAs.type": {"$ne": "ArtsdataIdentifier"}}},
-            {"$project": {"_id": 1}}
-        ])
+        {"$match": {"sameAs.type": {"$ne": "ArtsdataIdentifier"}}},
+        {"$project": {"_id": 1}}
+    ])
     return results
 
 def unlink_entities(client, headers):
     db = client['footlight-calendar']
     query = load_query('./sparql/unlink-entities.sparql')
-    data = {'query' : query}
+    data = {'query': query}
     data_encoded = urllib.parse.urlencode(data)
     response = post('https://db.artsdata.ca/repositories/artsdata', headers=headers, data=data_encoded)
     result = json.loads(response.text)
     data = result['results']['bindings']
     event_ids = [item['event']['value'].split('/')[-1] for item in data]
     repository = "events"
+    log_message("Unlinking Events in CMS to Artsdata entities has started!")
     for event_id in event_ids:
         id = ObjectId(event_id)
         db[repository].update_one(
             {"_id": id},
             {"$pull": {"sameAs": {"type": "ArtsdataIdentifier"}}}
         )
-        print(f"Event {event_id} unlinked")
-        log_to_datadog(f"Event {event_id} unlinked")
-
+        log_message(f"Event {event_id} unlinked")
+    log_message("Unlinking Events in CMS to Artsdata entities has completed!")
+    return event_ids
 
 def reconcile_entities(client, ids, entity_type, headers):
     db = client['footlight-calendar']
@@ -72,24 +72,24 @@ def reconcile_entities(client, ids, entity_type, headers):
     for key, value in result_dict.items():
         id = ObjectId(key.split(",")[0].split("/")[-1])
         update_result = db[entity_type].update_one({"_id": id, "sameAs.type": {"$ne": "ArtsdataIdentifier"}},
-                                                  {"$addToSet": {
-                                           "sameAs": {"uri": value, "type": "ArtsdataIdentifier"}}})
-        if (update_result.raw_result['updatedExisting']):
-            print(f"Updated entity {id}")
-            log_to_datadog(f"Updated entity {id}")
+                                                   {"$addToSet": {
+                                                       "sameAs": {"uri": value, "type": "ArtsdataIdentifier"}}})
+        if update_result.raw_result['updatedExisting']:
+            log_message(f"Linked {entity_type} with id {id} to {value}")
 
 
-def log_to_datadog(message):
+def log_message(message):
+    print(message)
     log_body = HTTPLog(
-    [
-        HTTPLogItem(
-            ddsource="aggregator",
-            ddtags="sync-ids",
-            hostname="production-server-7",
-            message=message,
-            service="footlight-aggregator", 
-        ),
-    ]
+        [
+            HTTPLogItem(
+                ddsource="aggregator",
+                ddtags="sync-ids",
+                hostname="production-server-7",
+                message=message,
+                service="footlight-aggregator",
+            ),
+        ]
     )
     configuration = Configuration()
     with ApiClient(configuration) as api_client:
@@ -103,7 +103,6 @@ if __name__ == '__main__':
     else:
         client = MongoClient("mongodb://localhost:27017")
 
-    
     headers = {
         'Accept': 'application/x-sparqlstar-results+json, application/sparql-results+json;q=0.9, */*;q=0.8',
         'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
@@ -113,12 +112,13 @@ if __name__ == '__main__':
     }
 
     collections = ["organizations", "places", "events", "people"]
+    linked_entities = []
     for collection in collections:
-        print(f"Updating {collection} started!")
-        log_to_datadog(f"Updating {collection} started!")
+        log_message(f"Linking {collection} CMS to Artsdata entities has started!")
         ids = get_entity_ids(client, collection)
-        reconcile_entities(client, ids,collection, headers)
-        print(f"Updating {collection} completed!\n")
-        log_to_datadog(f"Updating {collection} completed!")
+        linked_entities.extend(ids)
+        reconcile_entities(client, ids, collection, headers)
+        log_message(f"Linking {collection} CMS to Artsdata entities has completed!")
 
-    unlink_entities(client, headers)
+    unlinked_entity_ids = unlink_entities(client, headers)
+    log_message(f"Import statistics: Linked entities: {len(linked_entities)} Unlinked entities: {len(unlinked_entity_ids)}")
